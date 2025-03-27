049f8cd805fe90866e6dd5b22b5b87cea58f301b
rekofke <rekofke.eric@gmail.com>
2025-03-26 23:58:32 -0700

    updated homepage with hero image, changed fonts, and gave some styling to css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
-const Home = () => {
-    return (
-        <>
-            <h1 classNam="mt-5">Welcome to my Store</h1>
-        </>
+import { Image } from "react-bootstrap";
 
-    )
-}
+const Home = () => {
+  return (
+    <div className="home-hero">
+      <h3>Welcome to</h3>
+      <h1>Rekode Distributors</h1>
+      <Image
+        src="/src/assets/mainimg.avif"
+        className="main-image"
+        alt="Distribution network"
+      />
+    </div>
+  );
+};
 
 export default Home;
