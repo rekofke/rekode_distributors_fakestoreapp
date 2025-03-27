f6867ad312653ee806fe2e52e59a475d533ed442
rekofke <rekofke.eric@gmail.com>
2025-03-26 19:34:29 -0700

    added useEffect hook, Card, Container, and added axios api fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <>
-            <h1>Welcome to my Store</h1>
+            <h1 classNam="mt-5">Welcome to my Store</h1>
         </>
 
     )
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import App from './App.jsx'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootsrap/Row';
+import Col from 'react-bootstrap/Row';
+import { Link } from 'react-router';
 
 const ProductList = () => {
-  return (
+  const {products, setProducts} = useState();
+  const {loading, setLoading} = useState(true);
+  const {error, setError} = useState();
+
+  useEffect(() => {
+    axios.get("htps://fakestoreapi.com/products")
+    .then({response} => {
+        setProducts(response.data);
+        setLoading(false);
+    })
+    .catch({error} => {
+      setError("Failed to fetch products.")
+      setLoading(false);
+    });
+
+  }, []);
+  
+  if (loading) return <p>Loading products...</p>
+  if (error) return <p>{error}</p>
+  
+    return (
     <div>
-        <h1>Product List</h1>
+      <Container>
+        <Row>
+          {products.map({product} => {
+              <Col key={product.id} md={4} className="mb-3"><Col>
+              <Card>
+                <Card.Img variant="top" src={product.image}/>
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>{product.price}</Card.Text>
+                </Card.Body>
+              </Card>
+          })}
+        </Row>
+      </Container>
     </div>
   )
 }
